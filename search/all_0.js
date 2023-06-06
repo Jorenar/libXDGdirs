056c146cc547115d802dbce855a8a20b342ef897
Jorengarenar <Jorengarenar@users.noreply.github.com>
2023-06-06 08:04:44 +0000

    deploy: 5e9e90f1c97bb1edc212d78ef1368279295dc413

--- search/all_0.js
+++ search/all_0.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['changelog_2emd_0',['CHANGELOG.md',['../CHANGELOG_8md.html',1,'']]],
-  ['compilation_2edox_1',['compilation.dox',['../compilation_8dox.html',1,'']]],
-  ['contributing_2emd_2',['CONTRIBUTING.md',['../CONTRIBUTING_8md.html',1,'']]],
-  ['custom_2ec_3',['custom.c',['../custom_8c.html',1,'']]],
-  ['changelog_4',['Changelog',['../md_CHANGELOG.html',1,'']]],
-  ['contributing_20to_20libxdgdirs_5',['Contributing to libXDGdirs',['../md_CONTRIBUTING.html',1,'']]]
+  ['changelog_0',['Changelog',['../md_CHANGELOG.html',1,'']]],
+  ['changelog_2emd_1',['CHANGELOG.md',['../CHANGELOG_8md.html',1,'']]],
+  ['compilation_2edox_2',['compilation.dox',['../compilation_8dox.html',1,'']]],
+  ['contributing_20to_20libxdgdirs_3',['Contributing to libXDGdirs',['../md_CONTRIBUTING.html',1,'']]],
+  ['contributing_2emd_4',['CONTRIBUTING.md',['../CONTRIBUTING_8md.html',1,'']]],
+  ['custom_2ec_5',['custom.c',['../custom_8c.html',1,'']]]
 ];
--- search/all_5.js
+++ search/all_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['raw_12',['raw',['../structxdgDirsList__t.html#a92dbe4ee69a22e26818cf4c06c800680',1,'xdgDirsList_t']]],
-  ['readme_2emd_13',['README.md',['../README_8md.html',1,'']]],
-  ['reading_20environment_20variables_14',['Reading environment variables',['../group__VARIABLE__GETTERS.html',1,'']]]
+  ['reading_20environment_20variables_13',['Reading environment variables',['../group__VARIABLE__GETTERS.html',1,'']]],
+  ['readme_2emd_14',['README.md',['../README_8md.html',1,'']]]
 ];
