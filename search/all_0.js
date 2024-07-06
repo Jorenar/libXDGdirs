b93092deaa17896b18ef3848d66639bd4b27265a
Jorengarenar <Jorengarenar@users.noreply.github.com>
2024-07-06 04:25:29 +0000

    deploy: 44e04127aed2e6de7ccd70a80b822b14f8e31581

--- search/all_0.js
+++ search/all_0.js
@@ -7,8 +7,9 @@ var searchData=
   ['0_203_20v1_200_204_201_200_204_20a_202020_2012_2022_4',['&lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/compare/v1.0.3...v1.0.4&quot; &gt;1.0.4&lt;/a&gt; - 2020-12-22',['../md_CHANGELOG.html#autotoc_md11',1,'']]],
   ['0_204_20v1_200_205_201_200_205_20a_202022_2002_2018_5',['&lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/compare/v1.0.4...v1.0.5&quot; &gt;1.0.5&lt;/a&gt; - 2022-02-18',['../md_CHANGELOG.html#autotoc_md9',1,'']]],
   ['0_205_20v1_201_200_201_201_200_20a_202022_2004_2027_6',['&lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/compare/v1.0.5...v1.1.0&quot; &gt;1.1.0&lt;/a&gt; - 2022-04-27',['../md_CHANGELOG.html#autotoc_md6',1,'']]],
-  ['02_2018_7',['&lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/compare/v1.0.4...v1.0.5&quot; &gt;1.0.5&lt;/a&gt; - 2022-02-18',['../md_CHANGELOG.html#autotoc_md9',1,'']]],
-  ['04_2027_8',['&lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/compare/v1.0.5...v1.1.0&quot; &gt;1.1.0&lt;/a&gt; - 2022-04-27',['../md_CHANGELOG.html#autotoc_md6',1,'']]],
-  ['06_9',['[1.1.2] - 2024-07-06',['../md_CHANGELOG.html#autotoc_md1',1,'']]],
-  ['07_2006_10',['[1.1.2] - 2024-07-06',['../md_CHANGELOG.html#autotoc_md1',1,'']]]
+  ['0_20v1_201_201_201_201_201_20a_202023_2011_2011_7',['&lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/compare/v1.1.0...v1.1.1&quot; &gt;1.1.1&lt;/a&gt; - 2023-11-11',['../md_CHANGELOG.html#autotoc_md4',1,'']]],
+  ['02_2018_8',['&lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/compare/v1.0.4...v1.0.5&quot; &gt;1.0.5&lt;/a&gt; - 2022-02-18',['../md_CHANGELOG.html#autotoc_md9',1,'']]],
+  ['04_2027_9',['&lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/compare/v1.0.5...v1.1.0&quot; &gt;1.1.0&lt;/a&gt; - 2022-04-27',['../md_CHANGELOG.html#autotoc_md6',1,'']]],
+  ['06_10',['&lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/compare/v1.1.1...v1.1.2&quot; &gt;1.1.2&lt;/a&gt; - 2024-07-06',['../md_CHANGELOG.html#autotoc_md1',1,'']]],
+  ['07_2006_11',['&lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/compare/v1.1.1...v1.1.2&quot; &gt;1.1.2&lt;/a&gt; - 2024-07-06',['../md_CHANGELOG.html#autotoc_md1',1,'']]]
 ];
--- search/all_18.js
+++ search/all_18.js
@@ -7,8 +7,9 @@ var searchData=
   ['v1_200_203_20v1_200_204_201_200_204_20a_202020_2012_2022_4',['&lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/compare/v1.0.3...v1.0.4&quot; &gt;1.0.4&lt;/a&gt; - 2020-12-22',['../md_CHANGELOG.html#autotoc_md11',1,'']]],
   ['v1_200_204_20v1_200_205_201_200_205_20a_202022_2002_2018_5',['&lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/compare/v1.0.4...v1.0.5&quot; &gt;1.0.5&lt;/a&gt; - 2022-02-18',['../md_CHANGELOG.html#autotoc_md9',1,'']]],
   ['v1_200_205_20v1_201_200_201_201_200_20a_202022_2004_2027_6',['&lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/compare/v1.0.5...v1.1.0&quot; &gt;1.1.0&lt;/a&gt; - 2022-04-27',['../md_CHANGELOG.html#autotoc_md6',1,'']]],
-  ['v1_201_201_20v1_201_202_201_201_201_20a_202023_2011_2011_7',['&lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/compare/v1.1.1...v1.1.2&quot; &gt;1.1.1&lt;/a&gt; - 2023-11-11',['../md_CHANGELOG.html#autotoc_md4',1,'']]],
-  ['v1_201_202_20head_8',['&lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/compare/v1.1.2...HEAD&quot;',['../md_CHANGELOG.html#autotoc_md0',1,'']]],
-  ['variables_9',['Reading environment variables',['../group__VARIABLE__GETTERS.html',1,'']]],
-  ['ver_2ec_10',['ver.c',['../ver_8c.html',1,'']]]
+  ['v1_201_200_20v1_201_201_201_201_201_20a_202023_2011_2011_7',['&lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/compare/v1.1.0...v1.1.1&quot; &gt;1.1.1&lt;/a&gt; - 2023-11-11',['../md_CHANGELOG.html#autotoc_md4',1,'']]],
+  ['v1_201_201_20v1_201_202_201_201_202_20a_202024_2007_2006_8',['&lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/compare/v1.1.1...v1.1.2&quot; &gt;1.1.2&lt;/a&gt; - 2024-07-06',['../md_CHANGELOG.html#autotoc_md1',1,'']]],
+  ['v1_201_202_20head_9',['&lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/compare/v1.1.2...HEAD&quot;',['../md_CHANGELOG.html#autotoc_md0',1,'']]],
+  ['variables_10',['Reading environment variables',['../group__VARIABLE__GETTERS.html',1,'']]],
+  ['ver_2ec_11',['ver.c',['../ver_8c.html',1,'']]]
 ];
