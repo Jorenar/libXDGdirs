941335434c0fdd9aa6b1811d1dd8999dfc7fe5cf
Jorengarenar <Jorengarenar@users.noreply.github.com>
2024-07-06 04:17:59 +0000

    deploy: c1aefc31cd148048705ef4c6f6bf4174c95cd4f0

--- search/all_15.js
+++ search/all_15.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['sample_20code_0',['Write bug reports with detail, background, and sample code',['../md_CONTRIBUTING.html#autotoc_md28',1,'']]],
+  ['sample_20code_0',['Write bug reports with detail, background, and sample code',['../md_CONTRIBUTING.html#autotoc_md31',1,'']]],
   ['size_1',['size',['../structxdgDirsList__t.html#a5cede8325a3046b6418934e339796221',1,'xdgDirsList_t']]],
-  ['style_2',['Code style',['../md_CONTRIBUTING.html#autotoc_md29',1,'']]],
+  ['style_2',['Code style',['../md_CONTRIBUTING.html#autotoc_md32',1,'']]],
   ['system_20directories_3',['System directories',['../group__SYS__DIRS.html',1,'']]]
 ];
