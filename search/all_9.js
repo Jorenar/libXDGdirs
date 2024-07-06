076aae2ff295a2daecd9a874cd0c3912ca67ff99
Jorengarenar <Jorengarenar@users.noreply.github.com>
2024-07-06 04:23:09 +0000

    deploy: 730dbd9e5d7e4a76a5fd6bd474f6c7053d0832b4

--- search/all_9.js
+++ search/all_9.js
@@ -1,10 +1,10 @@
 var searchData=
 [
   ['data_20cache_20management_0',['Data cache management',['../group__CACHE__MANAGEMENT.html',1,'']]],
-  ['detail_20background_20and_20sample_20code_1',['Write bug reports with detail, background, and sample code',['../md_CONTRIBUTING.html#autotoc_md30',1,'']]],
+  ['detail_20background_20and_20sample_20code_1',['Write bug reports with detail, background, and sample code',['../md_CONTRIBUTING.html#autotoc_md31',1,'']]],
   ['directories_2',['directories',['../group__SYS__DIRS.html',1,'System directories'],['../group__USER__DIRS.html',1,'User directories']]],
-  ['discussions_20a_3',['Ask questions using &lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/discussions&quot; &gt;Discussions&lt;/a&gt;',['../md_CONTRIBUTING.html#autotoc_md28',1,'']]],
-  ['discussions_20discussions_20a_4',['Ask questions using &lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/discussions&quot; &gt;Discussions&lt;/a&gt;',['../md_CONTRIBUTING.html#autotoc_md28',1,'']]],
-  ['documentation_5',['Documentation',['../index.html#autotoc_md38',1,'']]],
-  ['download_6',['Download',['../index.html#autotoc_md35',1,'']]]
+  ['discussions_20a_3',['Ask questions using &lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/discussions&quot; &gt;Discussions&lt;/a&gt;',['../md_CONTRIBUTING.html#autotoc_md29',1,'']]],
+  ['discussions_20discussions_20a_4',['Ask questions using &lt;a href=&quot;https://github.com/Jorengarenar/libXDGdirs/discussions&quot; &gt;Discussions&lt;/a&gt;',['../md_CONTRIBUTING.html#autotoc_md29',1,'']]],
+  ['documentation_5',['Documentation',['../index.html#autotoc_md39',1,'']]],
+  ['download_6',['Download',['../index.html#autotoc_md36',1,'']]]
 ];
--- search/all_b.js
+++ search/all_b.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['fixed_0',['fixed',['../md_CHANGELOG.html#autotoc_md4',1,'Fixed'],['../md_CHANGELOG.html#autotoc_md9',1,'Fixed'],['../md_CHANGELOG.html#autotoc_md11',1,'Fixed'],['../md_CHANGELOG.html#autotoc_md14',1,'Fixed'],['../md_CHANGELOG.html#autotoc_md18',1,'Fixed'],['../md_CHANGELOG.html#autotoc_md22',1,'Fixed']]]
+  ['fixed_0',['fixed',['../md_CHANGELOG.html#autotoc_md5',1,'Fixed'],['../md_CHANGELOG.html#autotoc_md10',1,'Fixed'],['../md_CHANGELOG.html#autotoc_md12',1,'Fixed'],['../md_CHANGELOG.html#autotoc_md15',1,'Fixed'],['../md_CHANGELOG.html#autotoc_md19',1,'Fixed'],['../md_CHANGELOG.html#autotoc_md23',1,'Fixed']]]
 ];
