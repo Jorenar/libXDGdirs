79b6bb9e45b266476b346199321919f8cd9dce01
Jorenar <Jorenar@users.noreply.github.com>
2024-11-08 06:08:24 +0000

    deploy: c53e5c3155f97e76fdfbd540d107fc28309dfb89

--- search/all_15.js
+++ search/all_15.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['tag_20v1_200_200_201_200_200_20a_202020_2012_2020_0',['&lt;a href=&quot;https://github.com/Jorenar/libXDGdirs/releases/tag/v1.0.0&quot; &gt;1.0.0&lt;/a&gt; - 2020-12-20',['../md_CHANGELOG.html#autotoc_md26',1,'']]],
+  ['tag_20v1_200_200_201_200_200_20a_202020_2012_2020_0',['&lt;a href=&quot;https://github.com/Jorenar/libXDGdirs/releases/tag/v1.0.0&quot; &gt;1.0.0&lt;/a&gt; - 2020-12-20',['../md_CHANGELOG.html#autotoc_md27',1,'']]],
   ['test_2ec_1',['test.c',['../test_8c.html',1,'']]]
 ];
